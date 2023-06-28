1b60b1e5add2fa847890a23de2b6f915e4bb1327
velmurugansk <skvelmurugan985@gmail.com>
2023-06-28 08:40:51 +0530

    profile card design started

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { BASE_URL } from '../api/loginApi';
 
 const Profile = () => {
+
+  const userDetails = useSelector((state) => state.user.user);
+   
   return (
-    <div className='rounded -w-full p-3'>
-        Profile come here.
+    <div className='rounded -w-full p-2'>
+        <div className='flex justify-between items-center'>
+          <div className='flex items-center'>
+            <img src={`${BASE_URL}/uploads/${userDetails.picturePath}`} className='rounded-full' width="60" height="60" alt="profilepic" />
+            <p className='ml-2 font-bold uppercase'>{userDetails.firstName +' '+ userDetails.lastName}</p>
+          </div>
+        </div>
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -29,6 +29,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 
 
 // Storage
