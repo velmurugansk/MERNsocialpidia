1b7f1b20c7cb79fe95e7d3f39dc80c5179c2a38e
velmurugansk <skvelmurugan985@gmail.com>
2023-07-04 20:08:34 +0530

    profile card design

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { BASE_URL } from '../api/loginApi';
+import {GrLocation} from 'react-icons/gr';
+import {FaSuitcase} from 'react-icons/fa';
 
 const Profile = () => {
 
@@ -13,6 +15,24 @@ const Profile = () => {
             <img src={`${BASE_URL}/uploads/${userDetails.picturePath}`} className='rounded-full' width="60" height="60" alt="profilepic" />
             <p className='ml-2 font-bold uppercase'>{userDetails.firstName +' '+ userDetails.lastName}</p>
           </div>
+        </div>
+        <hr className='mt-1 mb-1'></hr>
+        <div>
+          <div className="flex items-center">
+            <GrLocation /> <span className='space-x-4'>{userDetails.location}</span>
+          </div>
+          <div className="flex items-center">
+            <FaSuitcase /> <span className='space-x-4'>{userDetails.occupation}</span>
+          </div>          
+        </div>
+        <hr className='mt-1 mb-1'></hr>
+        <div className='flex justify-between items-center'>
+          <p className='text-sm'>who's viewed your profile</p>
+          <span>{userDetails.viewedProfile}</span>
+        </div>
+        <div className='flex justify-between items-center'>
+          <p className='text-sm'>impression of your post</p>
+          <span>{userDetails.impressions}</span>
         </div>
     </div>
   )
